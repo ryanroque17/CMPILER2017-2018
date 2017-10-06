8f45e0c73ede142e8ff21f00d35217236428803b
ryanroque17 <ryanroque17@gmail.com>
2017-10-06 11:42:27 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var browserSync = require('browser-sync');
+
+//var bs = browserSync.create();
+
+app.get('', function(req, res){
+	res.sendfile(__dirname + '/index.html');
+})
+
+app.use('/', express.static(__dirname + '/'));
+
+app.use('/antlr4/', express.static(__dirname + '/antlr4/'));
+app.use('/ace_components/', express.static(__dirname + '/ace_components/'));
+app.use('/parser/', express.static(__dirname + '/parser/'));
+app.use('/views/', express.static(__dirname + '/views/'));
+app.use('/styles/', express.static(__dirname + '/styles/'));
+app.use('/scripts/', express.static(__dirname + '/scripts/'));
+
+
+//bs.init({
+//	proxy: "localhost:3000",
+//	files: ['**/**']
+//})
+
+app.listen(3000, function(){
+	console.log("Server started on port 3000");
+})
