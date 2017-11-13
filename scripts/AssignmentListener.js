f9c73cb1bc7c79dd8301c603bbbbf20c3d778e38
ryanroque17 <ryanroque17@gmail.com>
2017-11-14 03:11:33 +0800

    Assign semi-finished

--- scripts/AssignmentListener.js
+++ scripts/AssignmentListener.js
@@ -4,7 +4,6 @@ var QwertyParser = require('../generated-parser/QwertyParser');
 var QwertyListener = require('../generated-parser/QwertyListener').QwertyListener;
 var SymbolTable = require("/node_modules/symbol-table/stack");
 var s = SymbolTable();
-
 AssignmentListener = function(res) {
 	this.Res = res;
     QwertyListener.call(this);
@@ -32,21 +31,90 @@ AssignmentListener.prototype.enterVar_decl = function(ctx) {
 	var typeName = ctx.data_type().start.text;
 	// variable name
 	var varName = ctx.var_identifier_list().start.text;
-
+	// variable value
+	var varValue = ctx.var_identifier_list().getChild(1).getChild(1).getText();
+	
 	if(s.has(varName)) {
 		console.log("variable " + varName + " already in stack");
-	} else {
-		s.set(varName);
+	} else {		
+		s.set(varName, varValue);
+
+		console.log("value of " +varName+ ":" + s.get(varName));
 	}
 };
 
 // Enter a parse tree produced by QwertyParser#assignment_statement.
 QwertyListener.prototype.enterAssignment_statement = function(ctx) {
-	console.log(ctx);
+	
+	var varName = ctx.getChild(0).getText();
+	var varValue;
+	var heightDiff;
+	var height;
+	var varHeight;
+	if(!s.has(varName)) {
+		console.log("variable " + varName + " NOT in stack!");
+	}else{
+		height = s.height();
+		varHeight = s.getItsHeight(varName);
+		
+		if(height != varHeight){
+			heightDiff = height - varHeight;
+			for(var i=0; i<heightDiff ;i++){
+				s.pop();
+			}
+		}
+		if(ctx.getChild(1).getText() == "="){
+			varValue = ctx.getChild(2).getText();
+		}else if(ctx.getChild(1).getText() == "++"){
+			varValue = s.get(varName) + "+1";
+		}else if(ctx.getChild(1).getText() == "--"){
+			varValue = s.get(varName) + "-1";
+		}else if(ctx.getChild(1).getText() == "+="){
+			varValue = s.get(varName) + "+" + ctx.getChild(2).getText();
+		}else if(ctx.getChild(1).getText() == "-="){
+			varValue = s.get(varName) + "-" + ctx.getChild(2).getText();
+		}else if(ctx.getChild(1).getText() == "*="){
+			varValue = s.get(varName) + "*" + ctx.getChild(2).getText();
+		}else if(ctx.getChild(1).getText() == "/="){
+			varValue = s.get(varName) + "/" + ctx.getChild(2).getText();
+		}else if(ctx.getChild(1).getText() == "%="){
+			varValue = s.get(varName) + "%" + ctx.getChild(2).getText();
+		}
+		s.set(varName, varValue);
+
+		console.log("value of " +varName+ ":" + s.get(varName));
+		if(height != varHeight){
+			for(var i=0; i<heightDiff ;i++){
+				s.push();
+			}
+		}
+	}
+};
+
+/*//Enter a parse tree produced by QwertyParser#var_assignment_statement
+QwertyListener.prototype.enterVar_assignment_statement = function(ctx) {
+	console.log()
+	//console.log("ass fact" + ctx.getParent().getText());
+	if(ctx.assignment_factor!=null)
+		console.log("ass fact" + ctx.assignment_factor());
+
 	var varName = ctx.start.text;
 	if(!s.has(varName)) {
 		console.log("variable " + varName + " NOT in stack!");
 	}
+};
+*/
+
+//Enter a parse tree produced by QwertyParser#assignment_statement.
+QwertyListener.prototype.enterAssignment_factor = function(ctx) {
+	//console.log("ass fact" + ctx.getParent().getText());
+	/*if(ctx.assignment_factor!=null)
+		console.log("ass fact" + ctx.assignment_factor());
+
+	var varName = ctx.start.text;
+	if(!s.has(varName)) {
+		console.log("variable " + varName + " NOT in stack!");
+	}*/
 };
 
 // Enter a parse tree produced by QwertyParser#if_statement.
@@ -88,5 +156,10 @@ AssignmentListener.prototype.enterFor_statement = function(ctx) {
 
 // Exit a parse tree produced by QwertyParser#for_statement.
 AssignmentListener.prototype.exitFor_statement = function(ctx) {
+	s.pop();
+};
+
+//Exit a parse tree produced by QwertyParser#program.
+AssignmentListener.prototype.exitProgram = function(ctx) {
 	s.pop();
 };
